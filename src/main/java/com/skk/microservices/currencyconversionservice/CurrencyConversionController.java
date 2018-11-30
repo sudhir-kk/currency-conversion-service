692cfd2e5e925694c3dd1f0a48967ccc0f5cd146
Kumar Kushwaha <s.a.kumar.kushwaha@accenture.com>
2018-11-30 14:11:25 +0530

    Added dependency for RabitMQ and Zipkin server to see the distributed tracing log

--- src/main/java/com/skk/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/skk/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 	
@@ -39,6 +41,7 @@ public class CurrencyConversionController {
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
 			
 		CurrencyConversionBean response=currencyExchangeServiceProxy.retriveExchangeValue(from, to);
+		logger.info("SKK"+"{}",response);
 		return new CurrencyConversionBean(
 				response.getId(), 
 				from, 
